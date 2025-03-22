2749667702b7d419dc2632ad71d2d2ab13bc7dd4
David Orr <davidyorr@gmail.com>
2025-03-22 03:51:42 +0000

    Go to the Home screen after logging in

--- src/features/login/LoginScreen.vue
+++ src/features/login/LoginScreen.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
+import { useRouter } from "vue-router";
 import { supabase } from "../../utils/supabase";
 import { ref } from "vue";
 
+const router = useRouter();
+
 const email = ref("");
 const password = ref("");
+const errorMessage = ref("");
 
 async function handleLoginButtonClick() {
   const response = await supabase.auth.signInWithPassword({
@@ -12,6 +16,14 @@ async function handleLoginButtonClick() {
   });
 
   console.log(response);
+
+  if (response.data.user !== null) {
+    router.push("/");
+  }
+
+  if (response.error !== null) {
+    errorMessage.value = response.error.message;
+  }
 }
 
 async function handleLogoutButtonClick() {
@@ -21,13 +33,23 @@ async function handleLogoutButtonClick() {
 </script>
 
 <template>
-  <h1>login</h1>
+  <h1>Login</h1>
   <main>
-    <input id="email" v-model="email" />
-    <input id="password" v-model="password" type="password" />
+    <input id="email" v-model="email" placeholder="email" />
+    <input
+      id="password"
+      v-model="password"
+      placeholder="password"
+      type="password"
+    />
     <button @click="handleLoginButtonClick">Login</button>
     <button @click="handleLogoutButtonClick">Logout</button>
+    <p id="error-message">{{ errorMessage }}</p>
   </main>
 </template>
 
-<style></style>
+<style>
+#error-message {
+  color: var(--error-message-color);
+}
+</style>
