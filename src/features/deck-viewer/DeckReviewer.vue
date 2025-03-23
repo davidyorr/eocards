f67d3d44f5d06661cee0f36683868347c35c3900
David Orr <davidyorr@gmail.com>
2025-03-23 19:54:12 +0000

    Add component for reviewing the cards
    Can currently view the front content of the cards

---
+++ src/features/deck-viewer/DeckReviewer.vue
@@ -0,0 +1,77 @@
+<script setup lang="ts">
+import { Database } from "@/database.types";
+import { supabase } from "@/utils/supabase";
+import { onMounted, ref } from "vue";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+const deckId = Number.parseInt(String(route.params.id));
+
+const cards = ref<Array<Database["public"]["Tables"]["card"]["Row"]>>([]);
+
+const currentCardIndex = ref(0);
+
+onMounted(() => {
+  fetchCards();
+});
+
+async function fetchCards() {
+  console.log("fetching cards");
+  if (Number.isNaN(deckId)) {
+    return;
+  }
+  try {
+    const { data, error } = await supabase
+      .from("card")
+      .select()
+      .eq("deck_id", deckId);
+
+    if (error) {
+      console.error("Error fetching cards:", error);
+      return;
+    }
+    console.log("fetched cards", data);
+
+    cards.value = data;
+  } catch (error) {
+    console.error("Error fetching cards:", error);
+  }
+}
+
+function handlePreviousClick() {
+  if (currentCardIndex.value === 0) {
+    currentCardIndex.value = cards.value.length - 1;
+  } else {
+    currentCardIndex.value = currentCardIndex.value - 1;
+  }
+}
+
+function handleNextClick() {
+  if (currentCardIndex.value === cards.value.length - 1) {
+    currentCardIndex.value = 0;
+  } else {
+    currentCardIndex.value = currentCardIndex.value + 1;
+  }
+}
+</script>
+
+<template>
+  <h1>Deck Reviewer</h1>
+  <div class="cards" v-if="cards.length > 0">
+    <button @click="handlePreviousClick">prev</button>
+    <article class="card-content">
+      {{ cards[currentCardIndex].front_content }}
+    </article>
+    <button @click="handleNextClick">next</button>
+  </div>
+  <h2 v-else>no cards</h2>
+</template>
+
+<style scoped>
+.cards {
+  .card-content {
+    border: 1px solid blue;
+    height: 200px;
+  }
+}
+</style>
--- src/features/deck-viewer/DeckViewerScreen.vue
+++ src/features/deck-viewer/DeckViewerScreen.vue
@@ -1,8 +1,14 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import DeckEditor from "./DeckEditor.vue";
+import DeckReviewer from "./DeckReviewer.vue";
 
+const studyingDeck = ref(false);
 const editingDeck = ref(false);
+
+function handleStudyDeckClick() {
+  studyingDeck.value = true;
+}
 
 function handleEditDeckClick() {
   console.log("edit");
@@ -13,8 +19,10 @@ function handleEditDeckClick() {
 <template>
   <h1>Deck Viewer</h1>
   <main>{{ $route.params.id }}</main>
+  <button @click="handleStudyDeckClick">Study Deck</button>
   <button @click="handleEditDeckClick">Edit Deck</button>
   <DeckEditor v-if="editingDeck" />
+  <DeckReviewer v-if="studyingDeck" />
 </template>
 
 <style></style>
