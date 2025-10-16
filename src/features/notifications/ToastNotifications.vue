da8644cdfa42aeb783e290de69219df8b67fc4cc
David Orr <davidyorr@gmail.com>
2025-10-16 07:21:34 +0000

    Add animation to toast notifications

--- src/features/notifications/ToastNotifications.vue
+++ src/features/notifications/ToastNotifications.vue
@@ -4,17 +4,19 @@ import { notificationsStore } from "@/stores/notificationsStore";
 
 <template>
 	<div class="notifications">
-		<div
-			v-for="notification in notificationsStore.notifications"
-			:key="notification.id"
-			:class="{
-				notification: true,
-				success: notification.type === 'SUCCESS',
-				error: notification.type === 'ERROR',
-			}"
-		>
-			{{ notification.message }}
-		</div>
+		<TransitionGroup name="slide-up">
+			<div
+				v-for="notification in notificationsStore.notifications"
+				:key="notification.id"
+				:class="{
+					notification: true,
+					success: notification.type === 'SUCCESS',
+					error: notification.type === 'ERROR',
+				}"
+			>
+				{{ notification.message }}
+			</div>
+		</TransitionGroup>
 	</div>
 </template>
 
@@ -23,7 +25,7 @@ import { notificationsStore } from "@/stores/notificationsStore";
 	display: flex;
 	gap: 12px;
 	flex-direction: column;
-	position: absolute;
+	position: fixed;
 	bottom: 0;
 	width: 100%;
 	align-items: center;
@@ -45,6 +47,16 @@ import { notificationsStore } from "@/stores/notificationsStore";
 		&.error {
 			background-color: red;
 		}
+	}
+
+	.slide-up-enter-active,
+	.slide-up-leave-active {
+		transition: all 0.4s ease;
+	}
+	.slide-up-enter-from,
+	.slide-up-leave-to {
+		opacity: 0;
+		transform: translateY(20px);
 	}
 }
 </style>
