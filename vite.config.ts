2f61547d86da6d2b7092f90a29127cdfad60ab26
David Orr <davidyorr@gmail.com>
2025-11-22 09:51:49 +0000

    Configure PWA

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,41 @@ import path from "path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import Icons from "unplugin-icons/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
 	plugins: [
 		vue(),
 		Icons({
 			compiler: "vue3",
+		}),
+		VitePWA({
+			filename: "service-worker.js",
+			registerType: "autoUpdate",
+			includeAssets: ["favicon.ico", "apple-touch-icon.png", "favicon.svg"],
+			manifest: {
+				name: "Eocards",
+				short_name: "Eocards",
+				description: "To me, this deck has been memorized for centuries",
+				theme_color: "#ffffff",
+				background_color: "#ffffff",
+				display: "standalone",
+				orientation: "portrait",
+				icons: [
+					{
+						src: "web-app-manifest-192x192.png",
+						sizes: "192x192",
+						type: "image/png",
+						purpose: "any maskable",
+					},
+					{
+						src: "web-app-manifest-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+						purpose: "any maskable",
+					},
+				],
+			},
 		}),
 	],
 	resolve: {
